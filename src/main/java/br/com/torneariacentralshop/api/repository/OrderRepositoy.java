4f3f104063585b37aca1e0d67cea8b749d3b3488
unknown <athamsmenezes@gmail.com>
2025-05-02 16:51:43 -0300

    feat: cria as Querys

--- src/main/java/br/com/torneariacentralshop/api/repository/OrderRepositoy.java
+++ src/main/java/br/com/torneariacentralshop/api/repository/OrderRepositoy.java
@@ -1,11 +1,19 @@
 package br.com.torneariacentralshop.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.torneariacentralshop.api.entities.Order;
 
 @Repository
 public interface OrderRepositoy extends JpaRepository<Order, Integer>{
-
+	@Query(value = ("SELECT * FROM orders WHERE user_id = :user_id"), nativeQuery = true)
+	Order findByUser(@Param("user_id")int user_id);
+	
+	@Query(value = ("SELECT * FROM orders WHERE user_id = :user_id"), nativeQuery = true)
+	List<Order> findAllByUserId(@Param("user_id")int user_id);
 }
