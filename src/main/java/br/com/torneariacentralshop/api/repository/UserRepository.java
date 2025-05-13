2b897bd8ded8b19e3af47e682d9e15b9093b7490
unknown <athamsmenezes@gmail.com>
2025-05-13 20:59:05 -0300

    feat: cria o findByEmail

--- src/main/java/br/com/torneariacentralshop/api/repository/UserRepository.java
+++ src/main/java/br/com/torneariacentralshop/api/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package br.com.torneariacentralshop.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.torneariacentralshop.api.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository< User, Integer> {}
+public interface UserRepository extends JpaRepository< User, Integer> {
+	Optional<User> findByEmail(String email);
+}
