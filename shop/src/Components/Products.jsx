3c0d904bd333c583c26b581d66cddd83f43c5f3d
unknown <athamsmenezes@gmail.com>
2025-05-28 17:34:21 -0300

    feat: create componente para exibir produtos no home

--- shop/src/Components/Products.jsx
+++ shop/src/Components/Products.jsx
@@ -1,17 +1,17 @@
-import { CgShoppingCart } from "react-icons/cg";
-import { LiaCartPlusSolid } from "react-icons/lia";
+import { Link } from "react-router-dom";
+import Button from "./Button";
 
-function Products({products}){
-    return(
-        <div className="bg-white w-50 p-5 rounded-[0.625rem] shadow-sm shadow-slate-600 hover:border border-red-700">
-            <img src="../../public/Porca Sextavada.jpeg" className="hover:scale-[1.05] duration-200 ease-out" alt="Produto" />
-            <h3 className="font-bold text-center mb-5">{products.name}</h3>
-            <p className="text-center h-20">{products.description}</p>
+function Products({ products }) {
+    return (
+        <div className="bg-white w-60 2xl:w-70 p-5 rounded-[0.625rem] shadow-sm shadow-slate-600 hover:border border-red-700">
+            <Link to={`/produto/${products.id}`}>
+                <img src="../../public/Porca Sextavada.jpeg" className="hover:scale-[1.05] duration-200 ease-out" alt="Produto" />
+                <h3 className="font-bold text-center mb-5">{products.name}</h3>
+                <p className="text-center h-20">{products.description}</p>
+            </Link>
             <div className="flex gap-5 items-center justify-center">
                 <p className="font-bold">Por: {products.price}</p>
-                <div className="flex bg-[#1E4D05] rounded-[0.625rem] hover:scale-[1.05]">
-                    <button className="flex w-10 text-center justify-center"><LiaCartPlusSolid className="text-white text-3xl" /></button>
-                </div>
+                <Button />
             </div>
         </div>
     )
