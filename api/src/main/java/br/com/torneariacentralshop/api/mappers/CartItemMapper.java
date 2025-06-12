a859d167a87db2bedf0d163f7b34fe2394441343
unknown <athamsmenezes@gmail.com>
2025-06-12 20:53:38 -0300

    feat: adiciona id no retorno

--- api/src/main/java/br/com/torneariacentralshop/api/mappers/CartItemMapper.java
+++ api/src/main/java/br/com/torneariacentralshop/api/mappers/CartItemMapper.java
@@ -5,7 +5,7 @@ import br.com.torneariacentralshop.api.entities.CartItem;
 public class CartItemMapper {
 	
 	public static CartItemResponseDTO toDTO(CartItem cartItem) {
-		return new CartItemResponseDTO(CartMapper.toDTO(cartItem.getCart()), cartItem.getQuantity(), cartItem.getSubtotal(), ProductMapper.toDTO(cartItem.getProduct()));
+		return new CartItemResponseDTO(CartMapper.toDTO(cartItem.getCart()), cartItem.getId(), cartItem.getQuantity(), cartItem.getSubtotal(), ProductMapper.toDTO(cartItem.getProduct()));
 	}
 	
 	public static CartItem toEntity(CartItemResponseDTO cartDTO) {
