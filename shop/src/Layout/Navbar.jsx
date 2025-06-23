cf1f52de4e02dd03b323897fad8b065d79ba32b5
unknown <athamsmenezes@gmail.com>
2025-06-23 18:15:38 -0300

    refactor: alterando a forma de navegacao usando o window.location.href

--- shop/src/Layout/Navbar.jsx
+++ shop/src/Layout/Navbar.jsx
@@ -1,12 +1,19 @@
 import { IoMdSearch } from "react-icons/io";
 import { CgShoppingCart } from "react-icons/cg";
 import { PiUserCircleFill } from "react-icons/pi";
-import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
-function Navbar({ children }) {
-    const token = localStorage.getItem("token");
-    const decodeToken = jwtDecode(token)
-    const userName = decodeToken.name
+function Navbar({cartItem, userName}) { 
+    const navigate = useNavigate()
+    function cartClick(){
+        const token = localStorage.getItem('token')
+        console.log(token)
+        if (token === null) {
+            window.location.href ="/"
+        }else{
+            navigate("/cart")
+        }
+    }
     return (
         <div className="bg-[#BF1919] px-25 h-[8rem] flex items-center justify-between">
             <div className="flex items-center gap-3">
@@ -18,10 +25,10 @@ function Navbar({ children }) {
                 <button className="bg-[#1E4D05] rounded-[0.625rem] w-10 h-10 flex justify-center items-center"><IoMdSearch className="text-white text-2xl" /></button>
             </div>
             <div className="flex items-center gap-5">
-                <div className="relative">
-                    <div className="flex justify-end"><div className="flex bg-white rounded-full h-5 w-5 items-center justify-center mt-5 absolute translate-x-3 -translate-y-6"><p>0</p></div></div>
+                <button onClick={() => {cartClick()}} className="relative hover:scale-[1.05]">
+                    <div className="flex justify-end"><div className="flex bg-white rounded-full h-5 w-5 items-center justify-center mt-5 absolute translate-x-3 -translate-y-6"><p>{cartItem}</p></div></div>
                     <CgShoppingCart className="text-white text-4xl" />
-                </div>
+                </button>
                 <div>
                     <PiUserCircleFill className="text-white text-5xl" />
                     <p className="text-white text-center">{userName}</p>
