0463df2ffd843ade7f75c503ba9d8a627a5df44e
unknown <athamsmenezes@gmail.com>
2025-06-23 17:54:03 -0300

    feat: complementando a validacao do token

--- api/src/main/java/br/com/torneariacentralshop/api/auth/JwtFilter.java
+++ api/src/main/java/br/com/torneariacentralshop/api/auth/JwtFilter.java
@@ -42,8 +42,16 @@ public class JwtFilter extends OncePerRequestFilter{
 							new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
 					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 					SecurityContextHolder.getContext().setAuthentication(authToken);
+				}else {
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+	                response.getWriter().write("Token inválido");
+	                return;
 				}
+			}else {
+				logger.warn("Token JWT com e-mail nulo ou já autenticado.");
 			}
+		}else {
+			logger.debug("Authorization header ausente ou inválido.");
 		}
 			
 		filterChain.doFilter(request, response);
