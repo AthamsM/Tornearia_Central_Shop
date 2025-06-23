0054629d655686f2a665e8fa113a498bcd107d77
unknown <athamsmenezes@gmail.com>
2025-06-23 18:14:45 -0300

    feat: validacao do token com redirecionamento

--- shop/src/Controller/Api.jsx
+++ shop/src/Controller/Api.jsx
@@ -2,6 +2,34 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: "http://localhost:8080/api",
-   // headers :{Authorization : `Bearer ${localStorage.getItem('token')}`}
 })
-export default API
+
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+API.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            // Remove token inválido
+            localStorage.removeItem("token");
+            // Redireciona
+            window.location.href = "/";
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default API;
