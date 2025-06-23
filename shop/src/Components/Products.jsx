c4ec18bd7a9193ab42d2531d6d20b896110dbc9b
unknown <athamsmenezes@gmail.com>
2025-06-23 18:13:39 -0300

    feat: exibindo quantidade no produto

--- shop/src/Components/Products.jsx
+++ shop/src/Components/Products.jsx
@@ -8,7 +8,8 @@ function Products({ products }) {
             <Link to={`/produto/${products.id}`}>
                 <img src="../../public/Porca Sextavada.jpeg" className="hover:scale-[1.05] duration-200 ease-out" alt="Produto" />
                 <h3 className="font-bold text-center mb-5">{products.name}</h3>
-                <p className="text-center h-20">{products.description}</p>
+                <p className="text-center h-20 overflow-auto">{products.description}</p>
+                <p className="text-center"><span className="pr-2">Quantidade:</span>{products.stock}</p>
             </Link>
             <div className="flex gap-5 items-center justify-center">
                 <p className="font-bold">Por: {products.price}</p>
