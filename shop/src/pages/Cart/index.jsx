d28243c9a3e495ff7ff8210ab5e8bd7915de9bf6
unknown <athamsmenezes@gmail.com>
2025-07-03 21:01:58 -0300

    style: limpando o codigo

--- shop/src/pages/Cart/index.jsx
+++ shop/src/pages/Cart/index.jsx
@@ -1,17 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import CartItem from "../../Components/CartItem";
 import Template, { AppContext } from "../../Layout/Template";
 import { useContext } from "react";
 
 function Cart() {
     const { listCartItem, setListCartItem } = useContext(AppContext);
+    const navigate = useNavigate()
+
+    function payment(){
+        navigate("/checkout")
+    }
 
     return (
-        <div className="px-20 2xl:px-100 pt-10 flex gap-5">
+        <div className="px-20 2xl:px-80 pt-10 flex gap-5">
             <div>
                 <h1 className="font-bold text-2xl">Meu carrinho</h1>
                 {listCartItem.map(item => (
-                    <div className="pt-5">
-                        <CartItem key={item.cart_id} cartItem={item} setListCartItem={setListCartItem} />
+                    <div key={item.cart_id} className="pt-5">
+                        <CartItem cartItem={item} setListCartItem={setListCartItem} />
                     </div>
                 ))}
             </div>
@@ -21,10 +27,10 @@ function Cart() {
                         <h3>Total</h3>
                         <p>R$ {listCartItem.reduce((soma, sub) => soma + sub.subtotal, 0)}</p>
                     </div>
-                    <button className="mt-5 p-1 items-center justify-center rounded-[10px] font-bold text-white bg-[#BF1919]">Comprar</button>
+                    <button onClick={() => payment()} className="mt-5 p-1 items-center justify-center rounded-[10px] font-bold text-white bg-[#BF1919]">Comprar</button>
                 </div>
                 <div className="mt-5 w-full text-center hover:scale-[1.05] hover:text-[#BF1919]">
-                    <a href="/home" className="font-medium">Buscar mais peças</a>
+                    <a href="/home" className="font-medium">Continuar comprando</a>
                 </div>
             </div>
         </div>
